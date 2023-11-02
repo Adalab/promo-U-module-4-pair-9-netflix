58a4b59e7ab2d9605f629018c3bbebf2f1944346
susanamorillo <susana.des@gmail.com>
2023-11-02 11:23:33 +0100

    add back

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
@@ -10,4 +11,28 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'netflix',
+    password: 'Ainhoamiamor1',
+  });
+  connection.connect();
+
+  return connection;
+}
+
+// CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
+
+server.get('/movies', async (req, res) => {
+  const conn = await getConnection();
+
+  const queryMovies = 'SELECT * FROM movies';
+
+  const [results] = await conn.query(queryMovies);
+  res.json(results);
+  conn.end();
 });
